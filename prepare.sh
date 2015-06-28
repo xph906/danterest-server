9a6a976433e48c76ea0b80a569b8bd42d42df2c8
azure provisioned user <azureuser@danterest.danterest.h6.internal.cloudapp.net>
2015-06-28 19:40:57 +0000

    server

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+npm install express body-parser morgan mongoose jsonwebtoken --save
+npm install -g nodemon
+npm install cors --save
+npm install mysql
+npm install node-mysql --save
+npm install scribe-js --save 
+npm install jquery --save
+npm install sleep --save
+npm install jquery --save
+npm install extend --save
+npm install random-token --save
